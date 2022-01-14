4b28b2de0671a153e194aaad348b163247a6485c
Iurii Khvorost <>
2022-01-14 19:56:45 +0200

    Added: Package@swift-5.5.swift

---
+++ Package@swift-5.5.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "DLog",
+	platforms: [
+		.iOS(.v12),
+		.macOS(.v10_14),
+		.tvOS(.v12),
+		.watchOS(.v5)
+	],
+	products: [
+		.library(name: "DLog", targets: ["DLog"]),
+		.executable(name: "NetConsole", targets: ["NetConsole"])
+	],
+	targets: [
+		.target(name: "DLog"),
+		.target(name: "DLogObjC", dependencies: ["DLog"]),
+		.target(name: "NetConsole"),
+		.testTarget(name: "DLogTests", dependencies: ["DLog"]),
+		.testTarget(name: "DLogTestsObjC", dependencies: ["DLogObjC"]),
+	],
+	swiftLanguageVersions: [.v5]
+)
+
