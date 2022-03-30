1500540c64bec02fdc7a51907ca2e476f60a5229
Iurii Khvorost <iurii.khvorost@gmail.com>
2022-03-30 22:42:34 +0300

    isDebugger fix 2

--- Sources/DLog/LogPrivacy.swift
+++ Sources/DLog/LogPrivacy.swift
@@ -84,7 +84,11 @@ public enum LogPrivacy {
     }
     
     private static let isDebugger: Bool = {
-        getppid() != 1
+        var info = kinfo_proc()
+        var size = MemoryLayout.stride(ofValue: info)
+        var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+        sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
+        return (info.kp_proc.p_flag & P_TRACED) != 0
     }()
     
     private static let letters: [Character] = {
