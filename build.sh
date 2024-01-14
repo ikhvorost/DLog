2dc282a99206c8e5451ecfd7cee8475e4dfc9487
Iurii Khvorost <iurii.khvorost@gmail.com>
2024-01-14 16:10:24 +0200

    Build script update

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 set -e
 
-platforms=(
-  "platform=macOS" 
+SCHEME=$1
+BUILD_DIR=.build
+PLATFORMS=(
   "generic/platform=iOS"
-  #"generic/platform=iOS Simulator" 
+  "generic/platform=iOS Simulator" 
+  #"platform=macOS" 
   #"generic/platform=tvOS" 
   #"generic/platform=tvOS Simulator" 
   #"generic/platform=visionOS" 
@@ -12,9 +14,11 @@ platforms=(
   #"generic/platform=watchOS Simulator"
 )
 
-rm -rf build
+rm -rf $BUILD_DIR
 
-for i in "${platforms[@]}"; do
-  echo "\033[1mBuilding: ${i}\033[0m"
-  xcodebuild -quiet -scheme 'DLog-Package' -destination "$i" -derivedDataPath build -configuration Release
+for platform in "${PLATFORMS[@]}"; do
+  for config in Debug Release; do
+    echo "\033[1m$SCHEME ${config} ${platform}\033[0m"
+    xcodebuild -quiet -scheme $SCHEME -destination "$platform" -derivedDataPath $BUILD_DIR -configuration "$config"
+  done
 done
